d0e17aee0ddd2f0435bc736782207ad52c857197
Amit-rajput97 <84862054+Amit-rajput97@users.noreply.github.com>
2022-09-06 09:34:02 +0530

    Time: 1 ms (100.00%), Space: 39.2 MB (99.39%) - LeetHub

--- 231-power-of-two/231-power-of-two.java
+++ 231-power-of-two/231-power-of-two.java
@@ -6,10 +6,7 @@ class Solution {
         while(n%2==0)
             n /= 2;
         
-        if(n==1)
-            return true;
-        return false;
-        
+            return n==1;        
         
     }
     
