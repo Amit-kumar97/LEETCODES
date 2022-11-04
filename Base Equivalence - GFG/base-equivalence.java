142e5405490d04a67467fca2f68ed33191ef4785
Amit-rajput97 <84862054+Amit-rajput97@users.noreply.github.com>
2022-11-04 23:15:00 +0530

    Added solution - LeetHub

---
+++ Base Equivalence - GFG/base-equivalence.java
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            String[] input = new String[2]; 
+            input = br.readLine().split(" "); 
+            int n = Integer.parseInt(input[0]); 
+            int m = Integer.parseInt(input[1]); 
+            Solution ob = new Solution();
+            System.out.println(ob.baseEquiv(n,m));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution
+{
+    String baseEquiv(int n, int m)
+    {
+        // code here
+        for(int base=2; base<=32; base++){
+            int count = m;
+            int val = n;
+            
+            while(val>0){
+                val = val/base;
+                count--;
+            }
+            if(count==0){
+                    return "Yes";
+                }
+        }
+        return "No";
+    }
+}
